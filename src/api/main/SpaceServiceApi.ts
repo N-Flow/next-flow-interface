05764fd16c8825c5118a961706b120ef2db6b74a
HRan2004 <2125764918@qq.com>
2024-10-03 19:29:01 +0800

    feat: 完善SpaceService全部接口

--- src/api/main/SpaceServiceApi.ts
+++ src/api/main/SpaceServiceApi.ts
@@ -1,5 +1,7 @@
 import Page from "../../interface/core/page/Page";
 import Screen from "../../interface/core/screen/Screen";
+import {StepChangeListener} from "../../interface/core/page/StepChangeListener";
+import {ScreenSize} from "../../interface/core/screen/ScreenSize";
 
 
 export default class SpaceServiceApi {
@@ -8,6 +10,21 @@ export default class SpaceServiceApi {
   
   static screen: Screen
   
+  static addStepChangeListener(listener: StepChangeListener): void {
+  }
   
+  static removeStepChangeListener(listener: StepChangeListener): void {
+  }
+  
+  static setScreenSize(size: ScreenSize): void {
+  }
+  
+  static isBoxMode(): boolean {
+    return false
+  }
+  
+  static isDarkTheme(): boolean {
+    return false
+  }
 
 }
