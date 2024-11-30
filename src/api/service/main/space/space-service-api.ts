73af497ffbdc5b3b2233dc1b4b4107e44ccf95bf
YingFeng256 <yf2627181812@gmail.com>
2024-11-30 14:46:25 +0800

    refactor: 函数添加public

--- src/api/service/main/space/space-service-api.ts
+++ src/api/service/main/space/space-service-api.ts
@@ -9,18 +9,18 @@ export default class SpaceServiceApi {
   static page: Page
   
   static screen: Screen
-  
-  static addStepChangeListener(listener: StepChangeListener): void {
+
+  public static addStepChangeListener(listener: StepChangeListener): void {
   }
   
-  static removeStepChangeListener(listener: StepChangeListener): void {
+  public static removeStepChangeListener(listener: StepChangeListener): void {
   }
   
-  static isBoxMode(): boolean {
+  public static isBoxMode(): boolean {
     return false
   }
   
-  static isDarkTheme(): boolean {
+  public static isDarkTheme(): boolean {
     return false
   }
 
