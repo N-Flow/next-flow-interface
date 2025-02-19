ef47edf6bb933afd365065469c49f501d848e244
HRan2004 <2125764918@qq.com>
2025-02-20 01:12:04 +0800

    feat: 支持 plugin data 数据传递

--- src/plugin/interface/plugin-view-props.interface.ts
+++ src/plugin/interface/plugin-view-props.interface.ts
@@ -3,6 +3,5 @@ import ViewPlugin from "@/plugin/view-plugin";
 import {DivProps} from "@/api/components/interface/base-types";
 
 export interface PluginViewProps extends DivProps {
-  data: any,
   plugin: DrawerPlugin | ViewPlugin,
 }
