3cc40723b2c140500cd04dc6a6d5784293f6be19
HRan2004 <2125764918@qq.com>
2025-02-26 23:49:59 +0800

    feat: 完成 FirstLoadService 全流程

--- src/api/service/main/file/first/first-load-service-api.ts
+++ src/api/service/main/file/first/first-load-service-api.ts
@@ -1,9 +1,23 @@
 import {FirstLoadServiceStatus} from "@/api/service/main/file/first/dto/first-load-service-status.enum";
 import {FirstLoadServiceSubscriber} from "@/api/service/main/file/first/dto/first-load-service-subscriber.type";
+import {LocalFile} from "@/api/service/main/file/local/dto/local-file.type";
 
 export default interface FirstLoadServiceApi {
 
   status: FirstLoadServiceStatus
+
+  localFileList: LocalFile[]
+
+  loadingNumber: number
+  loadedNumber: number
+  errorNumber: number
+  canceledNumber: number
+
+  totalSize: number
+  loadedSize: number
+
+  startTime: number
+
 
   start(): void
 
