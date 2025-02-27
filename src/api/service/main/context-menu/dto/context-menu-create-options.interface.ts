c8ce8bcddbdf627dccf1dc5a87b7a315607941dc
YingFeng256 <yf2627181812@gmail.com>
2025-02-28 07:12:57 +0800

    feat: 添加右键菜单复选框

--- src/api/service/main/context-menu/dto/context-menu-create-options.interface.ts
+++ src/api/service/main/context-menu/dto/context-menu-create-options.interface.ts
@@ -4,6 +4,7 @@ import {ThemeColor} from "@/api/service/main/theme/dto/theme-color.enum";
 export interface ContextMenuCreateOptions {
   id?: string, // 菜单唯一id
   lines?: MenuLineCreateOptions[]  // 菜单内容
+  showIcon?: boolean  //显示图标 默认false
   x?: number,  // 菜单所在x轴
   y?: number,  // 菜单所在y轴
   closeOnOutsideClick?: boolean,  // 点击外部后关闭 默认为 false
