31ea65e3e449e5c7a70df169f08084f0a9718d80
HRan2004 <2125764918@qq.com>
2025-02-27 09:20:53 +0800

    feat: 支持历史记录堆栈操作

--- src/api/service/sync/history-manager-api.ts
+++ src/api/service/sync/history-manager-api.ts
@@ -9,4 +9,18 @@ export default interface HistoryManagerApi {
 
   check(operate: string): void
 
+  stop(): void
+
+
+  clear(): void
+
+  clearUndoStack(): void
+
+  clearRedoStack(): void
+
+
+  removeFirst(operate?: string): boolean
+
+  removeLast(operate?: string): boolean
+
 }
