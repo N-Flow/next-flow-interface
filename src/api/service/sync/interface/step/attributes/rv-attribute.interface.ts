6e67ba50d1f785ee9985adcb51adecb3dda6881d
HRan2004 <2125764918@qq.com>
2025-03-16 09:53:37 +0800

    feat: 支持从oflow-space项目热编译interface

--- src/api/service/sync/interface/step/attributes/rv-attribute.interface.ts
+++ src/api/service/sync/interface/step/attributes/rv-attribute.interface.ts
@@ -1,5 +1,5 @@
 import RvLocation from "@/api/service/sync/interface/step/attributes/location/rv-location.interface";
-import {RvMaterial} from "@/api/service/sync/interface/step/attributes/material/rv-material.interface";
+import RvMaterial from "@/api/service/sync/interface/step/attributes/material/rv-material.interface";
 
 export default interface RvAttribute {
   location?: RvLocation
