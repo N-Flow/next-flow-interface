86b8375db3c9de42c735eb49b002a2438eeaad41
HRan2004 <2125764918@qq.com>
2025-03-17 17:32:26 +0800

    fix: 临时补充丢失的样式

--- src/api/components/rsd/rsd-number-list-props.ts
+++ src/api/components/rsd/rsd-number-list-props.ts
@@ -1,6 +1,7 @@
 import {DivProps} from "@/api/components/interface/base-types";
 
 export interface RsdNumberListProps extends DivProps {
+  style?: React.CSSProperties
   value: number[]
   keys: string[]
   onChangeIndex: (index: number, value: number | null) => void
