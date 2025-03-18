a2b7594646f84e489beb8831878bcfc01f4218d8
HRan2004 <2125764918@qq.com>
2025-03-18 10:40:05 +0800

    feat: 支持实时动画

--- src/api/service/sync/interface/step/attributes/rv-attribute.interface.ts
+++ src/api/service/sync/interface/step/attributes/rv-attribute.interface.ts
@@ -1,6 +1,7 @@
 import RvLocation from "@/api/service/sync/interface/step/attributes/location/rv-location.interface";
 import RvMaterial from "@/api/service/sync/interface/step/attributes/material/rv-material.interface";
 
+// TODO: 改名为 RvAttributes 相关文件名，及引用的地方一同修改  @YF
 export default interface RvAttribute {
   location?: RvLocation
   material?: RvMaterial
