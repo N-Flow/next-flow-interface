411fd3a12ebe8e446bba73aed5a7ef590a2be671
HRan2004 <2125764918@qq.com>
2025-03-18 09:49:40 +0800

    feat: 优化坐标相关属性生成

--- src/api/service/sync/interface/step/attributes/location/rv-location.interface.ts
+++ src/api/service/sync/interface/step/attributes/location/rv-location.interface.ts
@@ -3,7 +3,7 @@ import {V3, V4} from "@/type/general/vector.type";
 export default interface RvLocation {
 
   position: V3
-  rotation: V3
+  rotation?: V3
   nearMode: boolean,
   scale: V3
   rotationQuaternion?: V4
