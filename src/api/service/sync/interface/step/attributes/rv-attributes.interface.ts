07d5f306d7691cc1c8dec3e5358ce9f1980c68dc
YingFeng256 <yf2627181812@gmail.com>
2025-03-19 17:55:23 +0800

    feat: 修改未修改的变量名

--- src/api/service/sync/interface/step/attributes/rv-attributes.interface.ts
+++ src/api/service/sync/interface/step/attributes/rv-attributes.interface.ts
@@ -1,7 +1,6 @@
 import RvLocation from "@/api/service/sync/interface/step/attributes/location/rv-location.interface";
 import RvMaterial from "@/api/service/sync/interface/step/attributes/material/rv-material.interface";
 
-// TODO: 改名为 RvAttributes 相关文件名，及引用的地方一同修改  @YF
 export default interface RvAttributes {
   location?: RvLocation
   material?: RvMaterial
