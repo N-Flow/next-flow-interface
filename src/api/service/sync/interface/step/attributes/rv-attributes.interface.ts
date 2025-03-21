38d676752c907b546bf63e71ed8a16ec96a16b1a
YingFeng256 <yf2627181812@gmail.com>
2025-03-21 21:49:01 +0800

    feat: 初步支持label-drawer

--- src/api/service/sync/interface/step/attributes/rv-attributes.interface.ts
+++ src/api/service/sync/interface/step/attributes/rv-attributes.interface.ts
@@ -1,7 +1,9 @@
 import RvLocation from "@/api/service/sync/interface/step/attributes/location/rv-location.interface";
 import RvMaterial from "@/api/service/sync/interface/step/attributes/material/rv-material.interface";
+import RvLabel from "@/api/service/sync/interface/step/attributes/label/rv-label.interface";
 
 export default interface RvAttributes {
   location?: RvLocation
   material?: RvMaterial
+  label?: RvLabel
 }
