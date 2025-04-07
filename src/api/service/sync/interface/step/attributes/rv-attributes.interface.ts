79905e46bb0a0b49ea9e76396de8e9745f3b38c0
YingFeng256 <yf2627181812@gmail.com>
2025-04-07 22:38:27 +0800

    feat: 实现播放模式下点击模型跳转step功能

--- src/api/service/sync/interface/step/attributes/rv-attributes.interface.ts
+++ src/api/service/sync/interface/step/attributes/rv-attributes.interface.ts
@@ -2,10 +2,12 @@ import RvLocation from "@/api/service/sync/interface/step/attributes/location/rv
 import RvMaterial from "@/api/service/sync/interface/step/attributes/material/rv-material.interface";
 import RvLabel from "@/api/service/sync/interface/step/attributes/label/rv-label.interface";
 import RvAnimation from "@/api/service/sync/interface/step/attributes/animation/rv-animation.interface";
+import RvCode from "@/api/service/sync/interface/step/attributes/code/rv-code.interface";
 
 export default interface RvAttributes {
   location?: RvLocation
   material?: RvMaterial
   label?: RvLabel
   animation?: RvAnimation
+  code?: RvCode
 }
