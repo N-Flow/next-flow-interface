8cd6637b7f7a99e6f6f9e114dc574fe20a97a07d
YingFeng256 <yf2627181812@gmail.com>
2025-04-10 01:33:05 +0800

    feat: 实现material相关功能

---
+++ src/utils/data-utils.ts
@@ -0,0 +1,12 @@
+import {RvPath} from "rhine-var";
+import {PathValueArray} from "@/utils/dto/path-value-array";
+
+export default class DataUtils {
+
+  static recursPathValue(path: RvPath, value: any): PathValueArray {
+    const pvList: PathValueArray = []
+    pvList.push({path, value})
+    return pvList
+  }
+
+}
