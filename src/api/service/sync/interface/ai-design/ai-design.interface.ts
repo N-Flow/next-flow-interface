9baf65d677391598237d683fd791ece8d1ad9414
YingFeng256 <yf2627181812@gmail.com>
2025-04-15 22:15:56 +0800

    feat: 添加ai-design相关功能

---
+++ src/api/service/sync/interface/ai-design/ai-design.interface.ts
@@ -0,0 +1,12 @@
+
+
+export default interface RvAiDesign {
+  cid: string
+  dialogue: RvAiDesignDialogue[]
+  time: string
+}
+
+export interface RvAiDesignDialogue {
+  role: string
+  content: string
+}
