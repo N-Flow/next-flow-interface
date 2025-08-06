7f410cf076b34104aeda0da37ddbc20e57f935a3
HRan2004 <2125764918@qq.com>
2025-08-06 22:10:05 +0800

    feat: 新增注释

--- src/api/service/main/space/dto/page/page-mode.enum.ts
+++ src/api/service/main/space/dto/page/page-mode.enum.ts
@@ -1,7 +1,7 @@
 export enum PageMode {
-  DISPLAY = 'DISPLAY',
-  FULL = 'FULL',
-  HP_BOX = 'HP_BOX',
-  VR = 'VR',
-  MR = 'MR',
+  DISPLAY = 'DISPLAY',  // 正常显示
+  FULL = 'FULL',  // 3D 场景全屏 - 隐藏所有 UI 元素
+  HP_BOX = 'HP_BOX',  // 全息投影盒 - 隐藏所有 UI 元素 - 且四摄像头渲染
+  VR = 'VR',  // VR 模式
+  MR = 'MR',  // MR 模式
 }
