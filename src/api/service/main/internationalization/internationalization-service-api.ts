b07087e8f5ad2cb00f8ad3365115684b0a30602d
HRan2004 <2125764918@qq.com>
2025-09-05 02:24:19 +0800

    refactor: 优化播放器配置

--- src/api/service/main/internationalization/internationalization-service-api.ts
+++ src/api/service/main/internationalization/internationalization-service-api.ts
@@ -22,9 +22,6 @@ export default interface InternationalizationServiceApi {
   // 获取翻译器，wait为true则自动等待对应namespace加载成功
   getTranslator(namespace?: string, wait?: boolean): Promise<Translator>
 
-  // 通过字符串转换语言元组，不存在则返回undefined
-  getLocaleByString(locale: string): Locale | undefined
-
   // 监听messages变化
   subscribe(subscriber: NamespaceSubscriber): () => void
 
