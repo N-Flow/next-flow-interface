ef3e6802141d1f979a2cc31a5ccb57ee8945b626
HRan2004 <2125764918@qq.com>
2025-09-10 07:22:21 +0800

    feat: 支持class计算

--- src/api/components/normal/scroll-bar-props.ts
+++ src/api/components/normal/scroll-bar-props.ts
@@ -3,5 +3,7 @@ import { HTMLAttributes, ReactNode } from 'react'
 export interface ScrollBarProps extends Omit<HTMLAttributes<HTMLDivElement>, 'children'> {
   children: ReactNode | undefined
   className?: string
-  drawer?: boolean
+  innerClassName?: string
+  barPaddingStart?: string
+  barPaddingEnd?: string
 }
