235f117c8918db7de72a45c439bfbc8fbbf0dfa0
HRan2004 <2125764918@qq.com>
2025-09-17 11:45:14 +0800

    feat: 优化会议模式效果

--- src/api/service/sync/meeting/rv-meeting-service-api.ts
+++ src/api/service/sync/meeting/rv-meeting-service-api.ts
@@ -3,23 +3,9 @@ import { RecursiveMap, StoredRhineVar } from 'rhine-var'
 import RvMeeting from '@/api/service/sync/interface/meeting/rv-meeting.interface'
 
 export default interface RvMeetingServiceApi {
-  PACKAGE_INTERVAL: number
-
-  ANIMATION_DURATION: number
-
-  CLICK_STORAGE_TIME: number
-
-  CLICK_STORAGE_NUMBER: number
-
-  CLICK_ANIMATION_DURATION: number
-
-  CLICK_ALLOW_DELAY: number
-
   meetings: RecursiveMap<RvMeeting>
 
-  currentMeeting: StoredRhineVar<RvMeeting> | null
-
-  getRvMeetingById(id: string): StoredRhineVar<RvMeeting> | null
+  getRvMeeting(meetingId: string): StoredRhineVar<RvMeeting> | null
 
   generateId(): string
 
