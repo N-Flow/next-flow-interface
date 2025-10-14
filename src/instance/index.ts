321c8db527f4446251da9e935001cb2fe415822c
HRan2004 <2125764918@qq.com>
2025-10-14 19:42:02 +0800

    feat: 初步配置双导出

---
+++ src/instance/index.ts
@@ -0,0 +1,3 @@
+import InternationalizationServiceApi from '@/api/service/main/internationalization/internationalization-service-api'
+export const a = 0
+export let internationalizationService: InternationalizationServiceApi
