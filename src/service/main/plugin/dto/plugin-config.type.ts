a3f05387d79d51e8507e7cdd5e2896144e480abd
HRan2004 <2125764918@qq.com>
2025-10-23 01:51:59 +0800

    feat: 优化插件国际化数据结构

--- src/service/main/plugin/dto/plugin-config.type.ts
+++ src/service/main/plugin/dto/plugin-config.type.ts
@@ -1,15 +1,23 @@
 import { FunctionComponent } from 'react'
 
+import { Locale } from '@/service/main/internationalization/dto/locale'
+
 export interface PluginConfig {
   id: string
 
   url: string
 
+  entry: string
+  css?: string
+  messages?: {
+    path: string
+    support: Locale[]
+    default: Locale
+  }
+
   official?: boolean
   necessary?: boolean
   private?: boolean
-
-  css?: boolean
 
   information: {
     name: string
--- src/service/sync/interface/plugin/rv-plugin-debug.ts
+++ src/service/sync/interface/plugin/rv-plugin-debug.ts
@@ -1,8 +1,39 @@
+import { Locale } from '@/service/main/internationalization/dto/locale'
+
+/**
+ * RvPluginDebug
+ *
+ * Example:
+ * {
+ *   id: 'example-plugin',
+ *   name: 'Example Plugin',
+ *   enable: true
+ *
+ *   url: 'http://localhost:8080',
+ *   hmr: 'ws://localhost:8081',
+ *
+ *   entry: 'index.js',
+ *   css: 'index.css',
+ *   messages: {
+ *     path: 'messages'
+ *     support: ['en', 'zh'],
+ *     default: 'en'
+ *   }
+ * }
+ */
 export default interface RvPluginDebug {
   id: string
   name: string
   enable: boolean
+
   url: string
   hmr: string
-  css: boolean
+
+  entry: string
+  css: string
+  messages: {
+    path: string
+    support: Locale[]
+    default: Locale
+  }
 }
